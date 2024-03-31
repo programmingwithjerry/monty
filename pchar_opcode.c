82dec5a2ea0039198017918af9d21aaf3a09cc40
programmingwithjerry <eneyellowj@gmail.com>
2024-03-31 18:35:19 +0000

    ALX loves order

--- pchar_opcode.c
+++ pchar_opcode.c
@@ -16,23 +16,23 @@ void print_char(stack_t **head, unsigned int line_number)
 
 	/*Set the current pointer to point to the top of the stack*/
 	current = *head;
-
-	/*Check if the stack is empty*/
-	if (!current)
+	/*Check if the value is out of the ASCII range*/
+	/*Check if the value is out of the ASCII range*/
+	if (current->n > 127 || current->n < 0)
 	{
-		/*Print error message and exit if stack is empty*/
-		fprintf(stderr, "L%d: can't pchar, stack is empty\n", line_number);
+		/*Print error message and exit if value is out of range*/
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		fclose(shared_info.file);
 		free(shared_info.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 
-	/*Check if the value is out of the ASCII range*/
-	if (current->n > 127 || current->n < 0)
+	/*Check if the stack is empty*/
+	if (!current)
 	{
-		/*Print error message and exit if value is out of range*/
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		/*Print error message and exit if stack is empty*/
+		fprintf(stderr, "L%d: can't pchar, stack is empty\n", line_number);
 		fclose(shared_info.file);
 		free(shared_info.content);
 		free_stack(*head);
